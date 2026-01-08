9f8e035441404a6b88a92551d870d74a8417d026
ecodoobiz.com 성수 <dhlee@ecodoobiz.com>
2026-01-08 17:49:03 +0900

    feature : 스플래시 화면 추가

---
+++ app/src/main/java/com/ediapp/dhammapada/SplashActivity.kt
@@ -0,0 +1,45 @@
+package com.ediapp.dhammapada
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.ediapp.dhammapada.ui.theme.MyKeywordTheme
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyKeywordTheme {
+                SplashScreen()
+            }
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.splash_logo),
+            contentDescription = "Splash Screen"
+        )
+    }
+}
